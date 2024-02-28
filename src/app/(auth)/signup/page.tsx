0bf034db0a3e075016f27f518986a1517a7dc45a
kenziehh <ghaisani.nurani@gmail.com>
2024-02-28 10:22:43 +0700

    setup react query and add auth route

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function SignUp() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>home</h1>
+      <h1>signup</h1>
     </main>
   );
 }
