9d03af1fb2d1810b080efcba7ed859e1eb08cd05
kenziehh <ghaisani.nurani@gmail.com>
2024-03-14 16:15:40 +0700

    update input component style

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -30,14 +30,14 @@ const Input = ({
         type={type}
         name={name}
         id={name}
-        className={`block py-6 px-5 w-full rounded-lg text-lg ${bgColor()} border-[1px] border-gray-300 appearance-none focus:outline-none focus:ring-1 focus:border-blue-600 peer ${className}`}
+        className={`block py-5 px-5 w-full rounded-[60px] text-lg ${bgColor()} border-[1px] border-gray-300 appearance-none focus:outline-none focus:ring-1 focus:border-blue-600 peer ${className}`}
         placeholder=""
         onChange={onChange}
         required={required}
       />
       <label
         htmlFor={name}
-        className="w-fit px-1 mx-5 mt-5 peer-focus:font-medium absolute l2 text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2 z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        className="w-fit px-1 mx-6 mt-4 peer-focus:font-medium absolute l2 text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2 z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
       >
         {label}
       </label>
