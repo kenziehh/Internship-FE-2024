3875b77a1631d51cf38f16d5c32491d2ae39c93a
kenziehh <ghaisani.nurani@gmail.com>
2024-03-15 19:07:29 +0700

    add logo icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.png"></link>
+      </head>
       <ReactQueryProvider>
         <body className="bg-darkest">
           <NextAuthProvider>
